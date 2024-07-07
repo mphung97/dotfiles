dbb400db4504ec64bc4b1e49b521b8f2fb68f442
mphung97 <mphung97@github.com>
2024-07-07 19:58:09 +0700

    feat: new colorschemes

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -65,10 +65,11 @@ return {
       },
     },
   },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "moonfly",
     },
   },
 }
